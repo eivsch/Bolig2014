a3f75d4bd722b81cda55e50954c792ab47bfd552
eivsch <s198752@stud.hioa.no>
2014-03-24 11:48:43 +0100

    Funksjonalitet og orsjektmappe

---
+++ Boligformidleren/src/boligformidleren/Boligformidleren.java
@@ -0,0 +1,22 @@
+/*
+ * Gruppemedlemmer:
+ * Eivind Schulstad	(s198752)
+ * and open the template in the editor.
+ */
+
+package boligformidleren;
+
+/**
+ *
+ * @author Eivind
+ */
+public class Boligformidleren {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
