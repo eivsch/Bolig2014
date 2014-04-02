100d239fc9eda75d38ac22e480d4d5485aaa9891
gretar80 <gretar80@gmail.com>
2014-04-02 10:58:47 +0200

    Enebolig.java og Leilighet.java - set og get metoder

--- Boligformidleren/src/boligformidleren/Enebolig.java
+++ Boligformidleren/src/boligformidleren/Enebolig.java
@@ -16,15 +16,42 @@ public class Enebolig extends Bolig {
     private int antEtasjer, tomtAreal;
     private boolean kjeller;
 
+    // konstruktør
     public Enebolig(String adresse, String type, String beskrivelse,
-            String annonsedato, int inneAreal, int antRom, int byggeaar, int leie,
+            String annonsedato, int inneAreal, int antRom, int byggeaar, int pris,
             int antEtasjer, int tomtAreal, boolean kjeller) {
         
         super(adresse, type, beskrivelse, annonsedato, inneAreal, antRom,
-                byggeaar, leie);
+                byggeaar, pris);
         this.antEtasjer = antEtasjer;
         this.tomtAreal = tomtAreal;
         this.kjeller = kjeller;
+    }
+    
+    // get-metoder
+    public int getAntEtasjer(){
+        return antEtasjer;
+    }
+    
+    public int getTomtAreal(){
+        return tomtAreal;
+    }
+    
+    public boolean getKjeller(){
+        return kjeller;
+    }
+    
+    // set-metoder
+    public void setAntEtasjer( int a ){
+        antEtasjer = a;
+    }
+    
+    public void setTomtAreal( int t ){
+        tomtAreal = t;
+    }
+    
+    public void setKjeller( boolean k ){
+        kjeller = k;
     }
     
     public String toString(){
--- Boligformidleren/src/boligformidleren/Leilighet.java
+++ Boligformidleren/src/boligformidleren/Leilighet.java
@@ -17,15 +17,41 @@ public class Leilighet extends Bolig {
     private String diverse;
     
     public Leilighet(String adresse, String type, String beskrivelse, 
-            String annonsedato, int inneAreal, int antRom, int byggeaar, int leie, 
+            String annonsedato, int inneAreal, int antRom, int byggeaar, int pris, 
             int etasje, boolean heis, boolean balkong, String diverse){
         
         super(adresse, type, beskrivelse, annonsedato, inneAreal, antRom,
-                byggeaar, leie);
+                byggeaar, pris);
         this.etasje = etasje;
         this.heis = heis;
         this.balkong = balkong;
         this.diverse = diverse;
+    }
+    
+    // get-metoder
+    public int getEtasje(){
+        return etasje;
+    }
+    
+    public boolean getHeis(){
+        return heis;
+    }
+    
+    public boolean getBalkong(){
+        return balkong;
+    }
+    
+    // set-metoder
+    public void setEtasje( int e ){
+        etasje = e;
+    }
+    
+    public void setHeis( boolean h ){
+        heis = h;
+    }
+    
+    public void setBalkong( boolean b ){
+        balkong = b;
     }
     
     public String toString(){
