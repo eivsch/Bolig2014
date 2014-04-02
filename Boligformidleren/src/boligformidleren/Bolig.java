965119c72a625ba6187a0e52d723bcf0f37d4682
gretar80 <gretar80@gmail.com>
2014-04-02 10:39:55 +0200

    Bolig.java - set og get metoder

--- Boligformidleren/src/boligformidleren/Bolig.java
+++ Boligformidleren/src/boligformidleren/Bolig.java
@@ -16,11 +16,12 @@ import java.io.*;
 public abstract class Bolig implements Serializable {
 
     private String adresse, type, beskrivelse, annonsedato;
-    private int inneAreal, antRom, byggeaar, leie;
+    private int inneAreal, antRom, byggeaar, pris;
     //bilde
 
+    // konstruktør
     public Bolig(String adresse, String type, String beskrivelse, 
-            String annonsedato, int inneAreal, int antRom, int byggeaar, int leie) {
+            String annonsedato, int inneAreal, int antRom, int byggeaar, int pris) {
 
         this.adresse = adresse;
         this.type = type;
@@ -29,14 +30,81 @@ public abstract class Bolig implements Serializable {
         this.inneAreal = inneAreal;
         this.antRom = antRom;
         this.byggeaar = byggeaar;
-        this.leie = leie;
+        this.pris = pris;
     }
+    
+    // get-metoder
+    public String getAdresse(){
+        return adresse;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public String getBeskrivelse(){
+        return beskrivelse;
+    }
+    
+    public String getAnnonsedato(){
+        return annonsedato;
+    }
+    
+    public int getInneAreal(){
+        return inneAreal;
+    }
+    
+    public int getAntRom(){
+        return antRom;
+    }
+    
+    public int getByggeaar(){
+        return byggeaar;
+    }
+    
+    public int getPris(){
+        return pris;
+    }
+    
+    // set-metoder
+    public void setAdresse( String a ){
+        adresse = a;
+    }
+    
+    public void setType( String t ){
+        type = t;
+    }
+    
+    public void setBeskrivelse( String b ){
+        beskrivelse = b;
+    }
+    
+    public void setAnnonsedato( String a ){
+        annonsedato = a;
+    }
+    
+    public void setInneAreal( int i ){
+        inneAreal = i;
+    }
+    
+    public void setAntRom( int a ){
+        antRom = a;
+    }
+    
+    public void setByggeaar( int b){
+        byggeaar = b;
+    }
+    
+    public void setPris( int p ){
+        pris = p;
+    }
+    
 
     public String toString() {
         String s = "Adresse: " + adresse + "\nType: " + type + "\nBeskrivelse: "
                 + beskrivelse + "\nAnnonsedato: " + annonsedato + "\nAreal: "
                 + inneAreal + "\nAntall rom: " + antRom + "\nByggeår: " + byggeaar
-                + "\nLeie: " + leie;
+                + "\nPris: " + pris;
 
         return s;
     }
