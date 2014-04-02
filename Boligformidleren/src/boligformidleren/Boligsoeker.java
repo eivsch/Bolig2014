8f99c0e8e6238df37964419ba25077f188093c2b
eivsch <s198752@stud.hioa.no>
2014-04-02 10:54:52 +0200

    ^^

--- Boligformidleren/src/boligformidleren/Boligsoeker.java
+++ Boligformidleren/src/boligformidleren/Boligsoeker.java
@@ -13,21 +13,22 @@ package boligformidleren;
 public class Boligsoeker extends Person {
 
     //Krav til bolig
-    private int leie, inneAreal, rom, byggeaar, antEtasjer;
+    private int pris, inneAreal, rom, byggeaar, antEtasjer;
     private String type;
     private boolean heis, balkong, kjeller;
 
     public Boligsoeker(String navn, String adresse, String epost, String type, int tlfnr,
-            int leie, int inneAreal, int rom, int byggeaar, int antEtasjer, boolean heis, 
+            int pris, int inneAreal, int rom, int byggeaar, int antEtasjer, boolean heis,
             boolean balkong, boolean kjeller) {
+        
         super(navn, adresse, epost, tlfnr);
         //Krav
         this.type = type;
-        this.leie = leie;
-        this. inneAreal = inneAreal;
+        this.pris = pris;
+        this.inneAreal = inneAreal;
         this.rom = rom;
         this.byggeaar = byggeaar;
-        this. antEtasjer = antEtasjer;
+        this.antEtasjer = antEtasjer;
         this.heis = heis;
         this.balkong = balkong;
         this.kjeller = kjeller;
